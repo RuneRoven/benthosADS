abfc10dc7b6112d4e57b6ae76dfdad38815b14bb
RuneRoven <daniel@mobynet.se>
2024-02-25 09:06:57 +0100

    changed library

--- ads/adsPlugin.go
+++ ads/adsPlugin.go
@@ -2,11 +2,13 @@ package adsPlugin
 
 import (
 	"context"
+	"time"
+	"sync"
 	"fmt"
 	"github.com/benthosdev/benthos/v4/public/service"
-	ads "gitlab.com/xilix-systems-llc/go-native-ads/v4" 
+	adsLib "gitlab.com/xilix-systems-llc/go-native-ads/v4" 
 )
-
+var waitGroup sync.WaitGroup
 
 // ads communication struct defines the structure for our custom Benthos input plugin.
 // It holds the configuration necessary to establish a connection with a Beckhoff PLC,
@@ -20,17 +22,17 @@ type adsCommInput struct {
 	cycleTime		int										// Read interval time if read type interval, cycle time if read type notification in milliseconds
 	maxDelay		int										// Max delay time after value change before PLC should send message, in milliseconds
 	timeout     	time.Duration                        	// Time duration before a connection attempt or read request times out.
-	handler     	*goADS.connection						// TCP handler to manage the connection.
+	handler     	*adsLib.Connection							// TCP handler to manage the connection.
 	log         	*service.Logger                       	// Logger for logging plugin activity.
 	symbols     	[]string 								// List of items to read from the PLC, grouped into batches with a maximum size.
-	deviceInfo 		DeviceInfo								// PLC device info 
-	deviceSymbols	ADSSymbol								// Received symbols from PLC
-	notificationChan chan *ads.Update
+	deviceInfo 		adsLib.DeviceInfo								// PLC device info 
+	deviceSymbols	adsLib.Symbol									// Received symbols from PLC
+	notificationChan chan *adsLib.Update
 }
 
 
 var adsConf = service.NewConfigSpec().
-	Summary("Creates an input that reads data from Beckhoff PLCs using ADS. Created by Daniel H & maintained by the United Manufacturing Hub. About us: www.umh.app").
+	Summary("Creates an input that reads data from Beckhoff PLCs using adsLib. Created by Daniel H & maintained by the United Manufacturing Hub. About us: www.umh.app").
 	Description("This input plugin enables Benthos to read data directly from Beckhoff PLCs using the ADS protocol. " +
 		"Configure the plugin by specifying the PLC's IP address, runtime port, target AMS net ID, etc. etc, add more here.").
 	Field(service.NewStringField("targetIP").Description("IP address of the Beckhoff PLC.")).
@@ -45,149 +47,175 @@ var adsConf = service.NewConfigSpec().
 	//Field(service.NewStringListField("symbols").Description("List of symbols and data type for each to read in the format 'function.name,int32', e.g., 'MAIN.counter,int32', '.globalCounter,int64' "))
 
 
-	func newAdsCommInput(conf *service.ParsedConfig, mgr *service.Resources) (service.Input, error) {
-		targetIP, err := conf.FieldString("targetIP")
-		if err != nil {
-			return nil, err
-		}
-	
-		targetAMS, err := conf.FieldString("targetAMS")
-		if err != nil {
-			return nil, err
-		}
-	
-		port, err := conf.FieldInt("port")
-		if err != nil {
-			return nil, err
-		}
-
-		hostAMS, err := conf.FieldString("hostAMS")
-		if err != nil {
-			return nil, err
-		}
-
-		readType, err := conf.FieldString("readType")
-		if err != nil {
-			return nil, err
-		}
-
-		cycleTime, err := conf.FieldInt("cycleTime")
-		if err != nil {
-			return nil, err
-		}
-		maxDelay, err := conf.FieldInt("maxDelay")
-		if err != nil {
-			return nil, err
-		}
-		
-		symbols, err := conf.FieldStringList("symbols")
-		if err != nil {
-			return nil, err
-		}
-	
-		timeoutInt, err := conf.FieldInt("timeout")
-		if err != nil {
-			return nil, err
-		}
-	
-		//for _, value := range slice {
-		//	fmt.Printf("Value: %s\n", value)
-		//}
-
-		m := &adsCommInput{
-			targetIP:   	targetIP,
-			targetAMS:  	targetAMS,
-			port:       	port,
-			hostAMS:		hostAMS,
-			readType: 		readType,
-			cycleTime:		cycleTime,
-			maxDelay:		maxDelay,
-			symbols:		symbols,
-			log:          	mgr.Logger(),
-			timeout:      	time.Duration(timeoutInt) * time.Second,
-		}
-	
-		return service.AutoRetryNacks(&m), nil
+func newAdsCommInput(conf *service.ParsedConfig, mgr *service.Resources) (service.Input, error) {
+	targetIP, err := conf.FieldString("targetIP")
+	if err != nil {
+		return nil, err
 	}
 
-	func init() {
-		err := service.RegisterBatchInput(
-			"adsComm", adsConf,
-			func(conf *service.ParsedConfig, mgr *service.Resources) (service.AckFunc, error) {
-				return newAdsCommInput(conf, mgr)
-			})
-		if err != nil {
-			panic(err)
-		}
+	targetAMS, err := conf.FieldString("targetAMS")
+	if err != nil {
+		return nil, err
 	}
 
-	func (g *adsCommInput) Connect(ctx context.Context) error { 
-		ctx, cancel := context.WithCancel(context.Background())
-		g.handler, e := goADS.NewConnection(ctx, g.targetIP, 48898, g.targetAMS, g.port, g.hostAMS, 10500)
-		//g.handler.Timeout = g.timeout
-		//g.handler.IdleTimeout = g.timeout
+	port, err := conf.FieldInt("port")
+	if err != nil {
+		return nil, err
+	}
+
+	hostAMS, err := conf.FieldString("hostAMS")
+	if err != nil {
+		return nil, err
+	}
+
+	readType, err := conf.FieldString("readType")
+	if err != nil {
+		return nil, err
+	}
+
+	cycleTime, err := conf.FieldInt("cycleTime")
+	if err != nil {
+		return nil, err
+	}
+	maxDelay, err := conf.FieldInt("maxDelay")
+	if err != nil {
+		return nil, err
+	}
 	
-		err := g.handler.Connect(false)
-		if err != nil {
-			g.log.Errorf("Failed to connect to  PLC at %s: %v", g.targetIP, err)
-			return err
-		}
-		defer g.handler.Close() // Close the connection when we are done
+	symbols, err := conf.FieldStringList("symbols")
+	if err != nil {
+		return nil, err
+	}
 
-		g.DeviceInfo, e := g.handler.ReadDeviceInfo()
-		if e != nil {
-			log.Critical(e)
-			os.Exit(1)
-		}
-		log.Infof("Successfully conncected to \"%s\" version %d.%d (build %d)", g.DeviceInfo.DeviceName, g.DeviceInfo.MajorVersion, g.DeviceInfo.MinorVersion, g.DeviceInfo.BuildVersion) /*}}}*/
+	timeoutInt, err := conf.FieldInt("timeout")
+	if err != nil {
+		return nil, err
+	}
 
-		//g.deviceSymbols, _ := g.handler.GetSymbol()
-		//g.handler.AddSymbolNotification("MAIN.I", update)
-		for _, symbolName := range g.symbols {
-			g.handler.AddSymbolNotification(symbolName, update)
-		}
-		/*
-		for _, symbolName := range g.symbols {
-			currentSymbol, ok := g.deviceSymbols[symbolName]
-			if !ok {
-				// Print an error if the symbol is missing
-				fmt.Printf("Error: Symbol %s not found\n", symbolName)
-				continue
-			}
-	
-			// Add device notification and set value based on your logic
-			currentSymbol.AddDeviceNotification(func(currentSymbol *goADS.ADSSymbol) {
-				val := g.handler.Value(symbolName)
-				if val == "True" {
-					connection.Set(symbolName, "1")
-				} else {
-					connection.Set(symbolName, "0")
-				}
-				currentSymbol.Walk()
-			})
-		}
-		*/
-		g.notificationChan = make(chan *ads.Update)
-		
+	//for _, value := range slice {
+	//	fmt.Printf("Value: %s\n", value)
+	//}
 
+	m := &adsCommInput{
+		targetIP:   	targetIP,
+		targetAMS:  	targetAMS,
+		port:       	port,
+		hostAMS:		hostAMS,
+		readType: 		readType,
+		cycleTime:		cycleTime,
+		maxDelay:		maxDelay,
+		symbols:		symbols,
+		log:          	mgr.Logger(),
+		timeout:      	time.Duration(timeoutInt) * time.Second,
+	}
+
+	return service.AutoRetryNacks(m), nil
+}
+
+func init() {
+	err := service.RegisterInput(
+		"adsinput", adsConf,
+		func(conf *service.ParsedConfig, mgr *service.Resources) (service.Input, error) {
+			return newAdsCommInput(conf, mgr)
+		})
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (g *adsCommInput) Connect(ctx context.Context) error {
+    // Create a new connection
+    var err error
+    g.handler, err = adsLib.NewConnection(ctx, g.targetIP, 48898, g.targetAMS, g.port, g.hostAMS, 10500)
+    if err != nil {
+        g.log.Errorf("Failed to create connection: %v", err)
+        return err
+    }
+
+    // Use a WaitGroup to wait for the goroutine to complete
+    var wg sync.WaitGroup
+    wg.Add(1)
+
+    // Use a mutex to synchronize access to shared variables
+    var mu sync.Mutex
+
+    // Connect to the PLC in a goroutine
+    go func() {
+        defer wg.Done()
+
+        // Wait for the context to be canceled before connecting
+        if ctx.Err() != nil {
+            g.log.Errorf("Connection operation canceled due to context deadline exceeded")
+            return
+        }
+
+        // Connect to the PLC
+        err := g.handler.Connect(false)
+        if err != nil {
+            g.log.Errorf("Failed to connect to PLC at %s: %v", g.targetIP, err)
+            return
+        }
+
+        // Read device info
+        g.log.Infof("Read device info")
+        g.deviceInfo, err = g.handler.ReadDeviceInfo()
+        if err != nil {
+            g.log.Errorf("Failed to read device info: %v", err)
+            return
+        }
+        g.log.Infof("Successfully connected to \"%s\" version %d.%d (build %d)", g.deviceInfo.DeviceName, g.deviceInfo.Major, g.deviceInfo.Minor, g.deviceInfo.Version)
+
+        // Ensure the notificationChan is initialized
+        mu.Lock()
+        defer mu.Unlock()
+        if g.notificationChan == nil {
+            g.notificationChan = make(chan *adsLib.Update)
+        }
+
+        // Add symbol notifications
+        for _, symbolName := range g.symbols {
+            g.log.Infof("Adding symbol notification for %s", symbolName)
+            g.handler.AddSymbolNotification(symbolName, g.notificationChan)
+        }
+    }()
+
+    // Wait for the goroutine to complete and check for errors
+    wg.Wait()
+    if ctx.Err() != nil {
+        g.log.Errorf("Connection operation canceled due to context deadline exceeded")
+        return ctx.Err()
+    }
+
+    return nil
+}
+
+
+func (g *adsCommInput) Read(ctx context.Context) (*service.Message, service.AckFunc, error) {
+	//var res *adsCommInput.notificationChan	
+	if g.handler == nil {
+		// Handle the case where g.handler is nil, e.g., return an error
+		return nil, nil, fmt.Errorf("handler is nil")
+	}
+
+	res := <-g.notificationChan
+	//msgs := service.Message{}
+	message := service.NewMessage([]byte(res.Value))
+	//msgs = append(msgs, message)
+	return message, func(ctx context.Context, err error) error {
+	// Nacks are retried automatically when we use service.AutoRetryNacks
 		return nil
-	}
-	func (g *adsCommInput) adsResponse(ctx context.Context) (service.Message, service.AckFunc, error) {
-		var res *adsCommInput.notificationChan	
-		msgs := service.Message{}
-		msgs = append(msgs, message)
-		return msgs, func(ctx context.Context, err error) error {
-		// Nacks are retried automatically when we use service.AutoRetryNacks
-			return nil
-		}, nil
+	}, nil
+}
+
+
+
+
+func (g *adsCommInput) Close(ctx context.Context) error {
+	if g.handler != nil {
+		close(g.notificationChan)
+        g.handler.Close()
+        g.handler = nil
 	}
 
-	func (g *adsCommInput) Close(ctx context.Context) error {
-		if g.handler != nil {
-			g.handler.Close()
-			g.handler = nil
-			
-		}
-	
-		return nil
-	}
+	return nil
+}
