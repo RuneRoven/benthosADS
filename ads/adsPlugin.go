8ebdee372d2048a8639bd8a100cf38b63f4e7c5a
RuneRoven <daniel@mobynet.se>
2024-02-26 22:30:14 +0100

    added batch input

--- ads/adsPlugin.go
+++ ads/adsPlugin.go
@@ -4,11 +4,12 @@ import (
 	"context"
 	"time"
 	"sync"
+	"errors"
 	"fmt"
+	"encoding/json"
 	"github.com/benthosdev/benthos/v4/public/service"
 	adsLib "gitlab.com/xilix-systems-llc/go-native-ads/v4" 
 )
-var waitGroup sync.WaitGroup
 
 // ads communication struct defines the structure for our custom Benthos input plugin.
 // It holds the configuration necessary to establish a connection with a Beckhoff PLC,
@@ -47,7 +48,7 @@ var adsConf = service.NewConfigSpec().
 	//Field(service.NewStringListField("symbols").Description("List of symbols and data type for each to read in the format 'function.name,int32', e.g., 'MAIN.counter,int32', '.globalCounter,int64' "))
 
 
-func newAdsCommInput(conf *service.ParsedConfig, mgr *service.Resources) (service.Input, error) {
+func newAdsCommInput(conf *service.ParsedConfig, mgr *service.Resources) (interface{},, error) {
 	targetIP, err := conf.FieldString("targetIP")
 	if err != nil {
 		return nil, err
@@ -109,7 +110,13 @@ func newAdsCommInput(conf *service.ParsedConfig, mgr *service.Resources) (servic
 		timeout:      	time.Duration(timeoutInt) * time.Second,
 	}
 
-	return service.AutoRetryNacks(m), nil
+	if readType == "notification" {
+        return service.AutoRetryNacks(m), nil
+    } else if inputType == "interval" {
+        return service.AutoRetryNacksBatched(m), nil
+    } else {
+        return nil, errors.New("unsupported input type")
+    }
 }
 
 func init() {
@@ -122,6 +129,38 @@ func init() {
 		panic(err)
 	}
 }
+func init() {
+    conf := &service.ConfigSpec{
+        Fields: service.AllCommonConfigFields(),
+        // Add other config fields as needed
+    }
+
+    // Obtain the "type" field from the config
+    inputType, _ := conf.FieldString("readType")
+
+    // Register the appropriate input type based on the "type" field
+    if inputType == "notification" {
+        err := service.RegisterInput(
+			"adsinput", adsConf,
+			func(conf *service.ParsedConfig, mgr *service.Resources) (service.Input, error) {
+				return newAdsCommInput(conf, mgr)
+			})
+        if err != nil {
+            panic(err)
+        }
+    } else if inputType == "interval" {
+        err := service.RegisterInput(
+			"adsinput", adsConf,
+			func(conf *service.ParsedConfig, mgr *service.Resources) (service.BatchInput, error) {
+				return newAdsCommInput(conf, mgr)
+			})
+        if err != nil {
+            panic(err)
+        }
+    } else {
+        panic("unsupported input type")
+    }
+}
 
 func (g *adsCommInput) Connect(ctx context.Context) error {
     // Create a new connection
@@ -131,89 +170,124 @@ func (g *adsCommInput) Connect(ctx context.Context) error {
         g.log.Errorf("Failed to create connection: %v", err)
         return err
     }
-
-    // Use a WaitGroup to wait for the goroutine to complete
-    var wg sync.WaitGroup
-    wg.Add(1)
-
     // Use a mutex to synchronize access to shared variables
     var mu sync.Mutex
 
-    // Connect to the PLC in a goroutine
-    go func() {
-        defer wg.Done()
+	// Connect to the PLC
+	err = g.handler.Connect(false)
+	if err != nil {
+		g.log.Errorf("Failed to connect to PLC at %s: %v", g.targetIP, err)
+		return err
+	}
+	
+	// Read device info
+	/*   g.log.Infof("Read device info")
+	g.deviceInfo, err = g.handler.ReadDeviceInfo()
+	if err != nil {
+		g.log.Errorf("Failed to read device info: %v", err)
+		return
+	}*/
+	// g.log.Infof("Successfully connected to \"%s\" version %d.%d (build %d)", g.deviceInfo.DeviceName, g.deviceInfo.Major, g.deviceInfo.Minor, g.deviceInfo.Version)
+	if g.readType == "notification" {
+		// Ensure the notificationChan is initialized
+		mu.Lock()
+		defer mu.Unlock()
+		if g.notificationChan == nil {
+			g.notificationChan = make(chan *adsLib.Update)
+		}
 
-        // Wait for the context to be canceled before connecting
-        if ctx.Err() != nil {
-            g.log.Errorf("Connection operation canceled due to context deadline exceeded")
-            return
-        }
-
-        // Connect to the PLC
-        err := g.handler.Connect(false)
-        if err != nil {
-            g.log.Errorf("Failed to connect to PLC at %s: %v", g.targetIP, err)
-            return
-        }
-		
-        // Read device info
-        g.log.Infof("Read device info")
-        g.deviceInfo, err = g.handler.ReadDeviceInfo()
-        if err != nil {
-            g.log.Errorf("Failed to read device info: %v", err)
-            return
-        }
-        g.log.Infof("Successfully connected to \"%s\" version %d.%d (build %d)", g.deviceInfo.DeviceName, g.deviceInfo.Major, g.deviceInfo.Minor, g.deviceInfo.Version)
-
-        // Ensure the notificationChan is initialized
-        mu.Lock()
-        defer mu.Unlock()
-        if g.notificationChan == nil {
-            g.notificationChan = make(chan *adsLib.Update)
-        }
-
-        // Add symbol notifications
-        for _, symbolName := range g.symbols {
-            g.log.Infof("Adding symbol notification for %s", symbolName)
-            g.handler.AddSymbolNotification(symbolName, g.notificationChan)
-        }
-		
-    }()
-
-    // Wait for the goroutine to complete and check for errors
-    wg.Wait()
-    if ctx.Err() != nil {
-        g.log.Errorf("Connection operation canceled due to context deadline exceeded")
-        return ctx.Err()
-    }
+		// Add symbol notifications
+		for _, symbolName := range g.symbols {
+			g.log.Infof("Adding symbol notification for %s", symbolName)
+			g.handler.AddSymbolNotification(symbolName, g.notificationChan)
+		}
+	}
 
     return nil
 }
 
+func (g *S7CommInput) ReadBatch(ctx context.Context) (service.MessageBatch, service.AckFunc, error) {
+	if g.client == nil {
+		return nil, nil, fmt.Errorf("PLC client is not initialized")
+	}
 
+	msgs := make(service.MessageBatch, 0)
+}
 func (g *adsCommInput) Read(ctx context.Context) (*service.Message, service.AckFunc, error) {
-	//var res *adsCommInput.notificationChan	
 	if g.handler == nil {
 		// Handle the case where g.handler is nil, e.g., return an error
 		return nil, nil, fmt.Errorf("handler is nil")
 	}
+/*
+	var res *adsLib.Update
+	select {
+	case res = <-g.notificationChan:
+		// Successfully received an update
+	case <-ctx.Done():
+		// Context canceled, return an error or handle accordingly
+		return nil, nil, fmt.Errorf("context canceled")
+	case <-time.After(time.Second): // Add a timeout to avoid blocking indefinitely
+		// Handle timeout, e.g., return an error or empty message
+		return nil, nil, fmt.Errorf("timeout waiting for update")
+	}
 
-	res := <-g.notificationChan
-	//msgs := service.Message{}
-	message := service.NewMessage([]byte(res.Value))
-	//msgs = append(msgs, message)
+	contentMap := map[string]interface{}{
+        "Variable":  res.Variable,
+        "Value":     res.Value,
+        "TimeStamp": res.TimeStamp,
+    }
+	// Convert the map to JSON
+    content, err := json.Marshal(contentMap)
+    if err != nil {
+        // Handle the error, e.g., log or return an error
+        return nil, nil, fmt.Errorf("failed to marshal update content: %v", err)
+    }
+
+    // Create a new message with the structured content
+    message := service.NewMessage(content)
+*/
+message := service.NewMessage(nil)
+	if g.readType == "interval" {
+		for _, symbolName := range g.symbols {
+			g.log.Infof("reading symbol  %s", symbolName)
+			res, _ := g.handler.ReadFromSymbol(symbolName)
+			contentMap := map[string]interface{}{
+				"Variable":  symbolName,
+				"Value":     res,
+			}
+			// Convert the map to JSON
+			content, err := json.Marshal(contentMap)
+			if err != nil {
+				// Handle the error, e.g., log or return an error
+				return nil, nil, fmt.Errorf("failed to marshal update content: %v", err)
+			}
+		
+			// Create a new message with the structured content
+			message = service.NewMessage(content)
+		}
+	}
 	return message, func(ctx context.Context, err error) error {
-	// Nacks are retried automatically when we use service.AutoRetryNacks
+		// Nacks are retried automatically when we use service.AutoRetryNacks
 		return nil
 	}, nil
 }
 
 
 
-
 func (g *adsCommInput) Close(ctx context.Context) error {
 	if g.handler != nil {
-		close(g.notificationChan)
+		if g.readType == "notification" {
+		for _, symbolName := range g.symbols {
+			handle, err := g.handler.GetHandleByName(symbolName)
+			if err != nil {
+				g.log.Errorf("Failed to get handle for symbol %s", symbolName)
+				return nil
+			}
+			g.handler.DeleteDeviceNotification(handle)
+        }
+		}else {
+			close(g.notificationChan)
+		}
         g.handler.Close()
         g.handler = nil
 	}
